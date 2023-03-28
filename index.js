befe965e7c5796918e49a942b65e152e14824298
unknown <hammadbro6@gmail.com>
2023-03-28 12:07:06 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// let tablevalue = 2
+// let i=1;
+// while (i<=10){
+    // console.log("2","x",i,"=", i*tablevalue);i= i+1
+
+    // let tablevalue=2;
+    // for (let i=1;i<=10;i=i+1){
+    //     console.log("2","x",i,"=",i*tablevalue)
+    // }
+
+    let string_value=""
+    while(string_value !== "okay")  
+   string_value= prompt ("we are using your data type okay to continue")
+    
+ 
+    
+    
+
