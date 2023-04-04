38912b1023776ee020ded45ba9a3d29948f3a8fa
unknown <hammadbro6@gmail.com>
2023-04-04 14:08:45 -0700

    first commit

--- index.js
+++ index.js
@@ -8,15 +8,23 @@
     //     console.log("2","x",i,"=",i*tablevalue)
     // }
 
-    let string_value=""
-    while(string_value !== "okay")  
-   string_value= prompt ("we are using your data type okay to continue")
+//     let string_value=""
+//     while(string_value !== "okay")  
+//    string_value= prompt ("we are using your data.Type okay to continue")
 
-   let sum=0;
-   for (let i=1;i<=10;i=i+1){
-    sum += i;
-   }
-   console.log(sum)
+//    let sum=0;
+//    for (let i=1;i<=10;i=i+1){
+//     sum += i;
+//    }
+//    console.log(sum)
+
+let arraynumbers = [1,2,3,4,5,6,7,8,9,10]
+let num = 0
+for (let i=0;i<arraynumbers.length;i++){
+    num += arraynumbers[i];
+}
+console.log ("The sum of number is",num)
+
 
     
  
